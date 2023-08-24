c5dde7290aefde8b3feea937c0d96c37ddf28aba
Iamneo <techadmin@iamneo.ai>
2023-08-24 10:47:21 +0000

    Backup Commit

--- jspservlet-app-stringmania/src/main/java/utility/StringAdvanceMethod.java
+++ jspservlet-app-stringmania/src/main/java/utility/StringAdvanceMethod.java
@@ -5,7 +5,7 @@ public class StringAdvanceMethod {
 	private String input1;
     private String input2;
 
-    // Getters and setters for input1 and input2
+    
 
     public String getInput1() {
         return input1;
@@ -23,17 +23,17 @@ public class StringAdvanceMethod {
         this.input2 = input2;
     }
 
-    // Method to concatenate two strings
+    
     public String concatenateStrings() {
         return input1 + input2;
     }
 
-    // Method to split two strings
+    
     public String[] splitStrings() {
         return input1.split(input2);
     }
 
-    // Method to find the first character in the string
+    
     public String findFirstCharacter() {
         if (input1.length() > 0) {
             return String.valueOf(input1.charAt(0));
@@ -41,13 +41,13 @@ public class StringAdvanceMethod {
         return "";
     }
 
-    // Method to find the index of the string
+    
     public String findIndexOfString() {
         int index = input1.indexOf(input2);
         return String.valueOf(index);
     }
 
-    // Method to trim the input string
+    
     public String trimString() {
         return input1.trim();
     }
--- jspservlet-app-stringmania/src/main/java/utility/StringBoolean.java
+++ jspservlet-app-stringmania/src/main/java/utility/StringBoolean.java
@@ -2,49 +2,5 @@ package utility;
 
 public class StringBoolean {
 //	Enter the code here....
-private String input1;
-    private String input2;
 
-    // Getters and setters for input1 and input2
-
-    public String getInput1() {
-        return input1;
-    }
-
-    public void setInput1(String input1) {
-        this.input1 = input1;
-    }
-
-    public String getInput2() {
-        return input2;
-    }
-
-    public void setInput2(String input2) {
-        this.input2 = input2;
-    }
-
-    // Method to check if string1 contains string2
-    public boolean containsString() {
-        return input1.contains(input2);
-    }
-
-    // Method to check if the string starts with the given letter
-    public boolean startsWithLetter(String letter) {
-        return input1.startsWith(letter);
-    }
-
-    // Method to check if the string ends with the given letter
-    public boolean endsWithLetter(String letter) {
-        return input1.endsWith(letter);
-    }
-
-    // Method to check if two strings are equal
-    public boolean areStringsEqual() {
-        return input1.equals(input2);
-    }
-
-    // Method to check if two strings are equal ignoring case
-    public boolean areStringsEqualIgnoreCase() {
-        return input1.equalsIgnoreCase(input2);
-    }
 }
