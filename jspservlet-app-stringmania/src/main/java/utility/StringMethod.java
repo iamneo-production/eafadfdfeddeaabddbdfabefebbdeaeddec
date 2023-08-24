85a375050a264621e1655725591a3b7056a99902
Iamneo <techadmin@iamneo.ai>
2023-08-24 11:23:30 +0000

    Backup Commit

--- jspservlet-app-stringmania/src/main/java/utility/StringMethod.java
+++ jspservlet-app-stringmania/src/main/java/utility/StringMethod.java
@@ -2,5 +2,34 @@ package utility;
 
 public class StringMethod {
 //	Enter the code here....
+private String input;
+public String getInput() {
+return input;
+}
+public void setInput(String input) {
+this.input = input;
+}
+public String lower(String input)
+{
+return input.toLowerCase();
 
+}
+public String upper(String input)
+{
+return input.toUpperCase();
+
+}
+public String firstCharacter(String input)
+
+{
+return input.substring(0,1);
+}
+public String length(String input)
+{
+return String.valueOf(input.length());
+}
+public String trim(String input)
+{
+return input.trim();
+}
 }
